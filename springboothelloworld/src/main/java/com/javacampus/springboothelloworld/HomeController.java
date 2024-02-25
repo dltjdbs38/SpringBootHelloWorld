0662109de00365ca0dc5f6325b7d16e040f0779f
dltjdbs38 <aassdd38@naver.com>
2024-02-25 14:34:12 +0900

    home() 함수로 변경 / @GetMapping에서 @RequestMapping으로 변경

--- springboothelloworld/src/main/java/com/javacampus/springboothelloworld/HomeController.java
+++ springboothelloworld/src/main/java/com/javacampus/springboothelloworld/HomeController.java
@@ -1,11 +1,14 @@
 package com.javacampus.springboothelloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 @RestController // 추가
 public class HomeController {
-    @GetMapping(value="/home") // 추가
-	public String index(){
-		return "index() return : Hello World in URL /home";
+    // @GetMapping(value="/home") // 추가
+    @RequestMapping(value="/", method=RequestMethod.GET)
+	public String home(){
+		return "home() return : Hello World in URL /";
 	}
 }
